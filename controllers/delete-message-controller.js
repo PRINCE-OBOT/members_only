929a048cb6f3994fcbb1510e1e6a6c5f7b2db272
PRINCE-OBOT <princessamuel45@gmail.com>
2026-05-12 00:54:34 +0100

    feat: authenticate only admin can delete message

--- controllers/delete-message-controller.js
+++ controllers/delete-message-controller.js
@@ -1,12 +1,12 @@
 const query = require("../db/query");
 
 const deleteMessageController = async (req, res) => {
-  if (!req.user.isAdmin)
+  if (!req.user.isadmin)
     return res.status(401).json({ message: "Only admin can delete message" });
 
-  const id = req.params.id;
+  const id = Number(req.params.id);
 
-  query.deleteMessage({ id });
+  query.deleteMessage(id);
 
   res.json({ message: "Message deleted" });
   // res.redirect("/");
