4fd4d80053e066c61435f5d9edb4f165fe3a2b31
PRINCE-OBOT <princessamuel45@gmail.com>
2026-05-13 17:13:23 +0100

    Move logout button to message header section

--- controllers/is-authenticated-controller.js
+++ controllers/is-authenticated-controller.js
@@ -2,7 +2,7 @@ function isAuthenticatedController(req, res, next) {
   const path = req.path;
 
   if (path !== "/log-in" && path !== "/sign-up" && !req.isAuthenticated()) {
-    return res.status(401).json({ message: "Unauthorized" });
+    return res.status(401).render('index', { title: 'Log in', pageTemplate: 'login'});
   }
 
   next();
